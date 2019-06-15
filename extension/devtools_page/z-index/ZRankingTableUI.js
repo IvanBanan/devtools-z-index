8f3296bd0d52d6d04b0b4df2cd275d8928e2dc07
«IvanBanan» <ivan.power.banan@gmail.com>
2019-06-15 08:53:37 +0300

    zContext functional have been added.

--- extension/devtools_page/z-index/ZRankingTableUI.js
+++ extension/devtools_page/z-index/ZRankingTableUI.js
@@ -4,7 +4,7 @@ class ZRankingTableUI {
     this._onClickListener = (event) => {
       const elSelector = event.target.closest('[data-selector]');
       if (elSelector) {
-        const selector = elSelector.getAttribute('data-selector');
+        const selector = elSelector.getAttribute('data-selector') + '[zIndex-guid="' + elSelector.getAttribute('uuid-element') + '"]';
         if (typeof this.onSelect === 'function') {
           this.onSelect(selector);
         }
@@ -24,32 +24,41 @@ class ZRankingTableUI {
 
   async updateTable ({ ranking = [] }) {
     const html = ranking
-      .map((row) => this._buildTableRowHtml(row))
-      .join('');
+    .map((row,i) => this._buildTableRowHtml(row,i))
+    .join('');
     this.elTable.innerHTML = html;
   }
 
-  _buildTableRowHtml (row) {
+  _buildTableRowHtml (row,i) {
     const selector = [
-      row.tagName,
-      row.id ? `#${row.id}` : '',
-      row.classNames.length > 0 ? `.${row.classNames.join('.')}` : '',
+    row.tagName,
+    row.id ? `#${row.id}` : '',
+    row.classNames.length > 0 ? `.${row.classNames.join('.')}` : '',
     ].join('');
 
     const selectorHtml = [
-      this._buildElementTitleHtml(row.tagName, 'tagName'),
-      this._buildElementTitleHtml(row.id, 'id', '#'),
-      this._buildElementTitleHtml(row.classNames.join('.'), 'classes', '.'),
+    this._buildElementTitleHtml(row.tagName, 'tagName'),
+    this._buildElementTitleHtml(row.id, 'id', '#'),
+    this._buildElementTitleHtml(row.classNames.join('.'), 'classes', '.'),
     ].join('');
 
     const html = `
-      <tr>
-        <td class="rankingTableItem-zIndex">${row.zIndex}</td>
-        <td class="rankingTableItem-element">
-          <span class="rankingTableItem-selector"
-            data-selector="${selector}">${selectorHtml}</span>
-        </td>
-      </tr>
+    <tr>
+    <td class="rankingTableItem-num">${i+1}</td>
+    <td class="rankingTableItem-zIndex">${row.zIndex}</td>
+    <td class="rankingTableItem-element">
+    <span class="rankingTableItem-selector" uuid-element="${row.zIndexGuid}" title="${row.zIndexGuid}"
+    data-selector="${selector}">${selectorHtml}</span>
+    </td>
+    <td class="rankingTableItem-parent">
+    <span class="rankingTableItem-selector" uuid-element="${row.zCont.parentStackingContextGUID}" title="${row.zCont.parentStackingContextGUID}"
+    data-selector="${row.zCont.parentStackingContext}">${row.zCont.parentStackingContext}</span>
+    </td>
+    <td class="rankingTableItem-zIndex">${row.zCont.parentZIndex}</td>
+    <td class="rankingTableItem-relevant">
+    <span class="rankingTableItem-relevant">${row.zCont.relevant}</span>
+    </td>
+    </tr>
     `;
     return html;
   }
@@ -64,13 +73,172 @@ class ZRankingTableUI {
 
 ZRankingTableUI.buildRanking = (d = document) => {
   const ranking = Array.from(d.querySelectorAll('*'))
-    .map((el) => ({
+  .map((el, arr) => { 
+    if (!el.getAttribute('zIndex-guid')){
+      el.setAttribute('zIndex-guid', guid());
+    }
+    return {
       classNames: Array.from(el.classList),
       id: el.id,
+      zIndexGuid: el.getAttribute('zIndex-guid'),
       tagName: el.tagName.toLowerCase(),
       zIndex: Number(getComputedStyle(el).zIndex),
-    }))
-    .filter(({ zIndex }) => !Number.isNaN(zIndex))
-    .sort((r1, r2) => r2.zIndex - r1.zIndex);
+      zCont: zContext(el)
+    }
+  })
+  .filter(({ zIndex }) => !Number.isNaN(zIndex))
+  .sort((r1, r2) => (r1.zCont.relevant - r2.zCont.relevant) || (r2.zIndex - r1.zIndex) ||(r2.zCont.parentStackingContextGUID.substr(0,1).charCodeAt() - r1.zCont.parentStackingContextGUID.substr(0,1).charCodeAt()));
   return ranking;
+
+  function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
+
+  function zContext(el) {
+    var props = {},
+    //Also see: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Positioning/Understanding_z_index/The_stacking_context
+    getClosestStackingContext = function( nodeOrObject ) {
+      var node = nodeOrObject.node || nodeOrObject;
+
+      //the root element (HTML)
+      if( ! node || node.nodeName === 'HTML' || node.nodeName === '#document-fragment' ) {
+        return { node: document.documentElement, reason: 'root' };
+      }
+
+      var computedStyle = getComputedStyle( node );
+
+      // position: fixed
+      if ( computedStyle.position === 'fixed' ) {
+        return { node: node, reason: 'position: fixed' };
+      }
+
+      // positioned (absolutely or relatively) with a z-index value other than "auto",
+      if ( computedStyle.zIndex !== 'auto' && computedStyle.position !== 'static' ) {
+        return { node: node, reason: 'position: ' + computedStyle.position + '; z-index: ' + computedStyle.zIndex };
+      }
+
+      // elements with an opacity value less than 1.
+      if ( computedStyle.opacity !== '1' ) {
+        return { node: node, reason: 'opacity: ' + computedStyle.opacity };
+      }
+
+      // elements with a transform value other than "none"
+      if ( computedStyle.transform !== 'none' ) {
+        return { node: node, reason: 'transform: ' + computedStyle.transform };
+      }
+
+      // elements with a mix-blend-mode value other than "normal"
+      if ( computedStyle.mixBlendMode !== 'normal' ) {
+        return { node: node, reason: 'mixBlendMode: ' + computedStyle.mixBlendMode };
+      }
+
+      // elements with a filter value other than "none"
+      if ( computedStyle.filter !== 'none' ) {
+        return { node: node, reason: 'filter: ' + computedStyle.filter };
+      }
+
+      // elements with a perspective value other than "none"
+      if ( computedStyle.perspective !== 'none' ) {
+        return { node: node, reason: 'perspective: ' + computedStyle.perspective };
+      }
+
+      // elements with isolation set to "isolate"
+      if ( computedStyle.isolation === 'isolate' ) {
+        return { node: node, reason: 'isolation: ' + computedStyle.isolation };
+      }
+
+      // transform or opacity in will-change even if you don't specify values for these attributes directly
+      if( computedStyle.willChange === 'transform' || computedStyle.willChange === 'opacity' ) {
+        return { node: node, reason: 'willChange: ' + computedStyle.willChange };
+      }
+
+      // elements with -webkit-overflow-scrolling set to "touch"
+      if ( computedStyle.webkitOverflowScrolling === 'touch' ) {
+        return { node: node, reason: '-webkit-overflow-scrolling: touch' };
+      }
+
+      // a flex item with a z-index value other than "auto", that is the parent element display: flex|inline-flex,
+      if ( computedStyle.zIndex !== 'auto' ) {
+        var parentStyle = getComputedStyle( node.parentNode );
+        if ( parentStyle.display === 'flex' || parentStyle.display === 'inline-flex' ) {
+          return {
+            node: node,
+            reason: 'flex-item; z-index: ' + computedStyle.zIndex
+          };
+        }
+      }
+
+      return getClosestStackingContext( { node: node.parentNode, reason: 'not a stacking context' } );
+
+    },
+    shallowCopy = function( data ) {
+      var props = Object.getOwnPropertyNames( data );
+      var copy = { __proto__: null };
+      for( var i = 0; i < props.length; ++i ) {
+        copy[ props[ i ] ] = data[ props[ i ] ];
+      }
+      return copy;
+    },
+    generateSelector = function( element ) {
+      var selector, tag = element.nodeName.toLowerCase();
+      if( element.id ) {
+        selector = '#' + element.getAttribute( 'id' );
+      } else if( element.getAttribute( 'class' ) ) {
+        selector = '.' + element.getAttribute( 'class' ).split( ' ' ).join( '.' );
+      }
+      return selector ? tag + selector : tag;
+    },
+    getRelevant = function(element, relevant){
+      var buffPC = getClosestStackingContext(element).node;
+
+
+      if(buffPC ===element){
+        if ( element.nodeName !== 'HTML' ) {
+          buffPC = getClosestStackingContext( element.parentNode ).node;
+        }
+      }
+
+      if (buffPC.nodeName !== 'HTML'){
+        relevant += 1;
+        relevant = getRelevant(buffPC, relevant);
+      } 
+      return relevant;
+        
+    };
+    if( el && el.nodeType === 1 ) {
+      var closest = getClosestStackingContext( el );
+      var createsStackingContext = el === closest.node;
+      var reason = createsStackingContext ? closest.reason : 'not a stacking context';
+      var parentContext = closest.node;
+      var computedStyle = getComputedStyle( el );
+      var relevant = 0;
+      if ( createsStackingContext && el.nodeName !== 'HTML' ) {
+        parentContext = getClosestStackingContext( el.parentNode ).node;
+      }
+
+      var parentComputedStyle = getComputedStyle( parentContext );
+
+      relevant = getRelevant(el, relevant);
+
+      props = {
+        current: generateSelector( el ),
+        createsStackingContext: createsStackingContext,
+        createsStackingContextReason: reason,
+        parentStackingContext: generateSelector( parentContext ),
+        parentStackingContextGUID: parentContext.getAttribute('zIndex-guid'),
+        parentZIndex: parentComputedStyle.zIndex !== 'auto' ? parseInt( parentComputedStyle.zIndex, 10 ) : parentComputedStyle.zIndex,
+        'z-index': computedStyle.zIndex !== 'auto' ? parseInt( computedStyle.zIndex, 10 ) : computedStyle.zIndex,
+        relevant: relevant
+      };
+    }
+    return shallowCopy( props );
+  }
+
+
 };
